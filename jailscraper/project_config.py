58ae81b3daa7828329cb2eac8b3899aa9239179d
David Eads <davideads@gmail.com>
2017-06-08 15:02:28 -0400

    move some configuration over to configuration file

--- jailscraper/project_config.py
+++ jailscraper/project_config.py
@@ -2,6 +2,15 @@
 import os
 
 PROJECT_SLUG = 'cookcountyjail2'
+INMATE_URL_TEMPLATE = 'http://www2.cookcountysheriff.org/search2/details.asp?jailnumber={0}'
+
+"""Sets the maximum jail number to scan for by default.
+
+If the subsequent jail number returns a 2xx status code, it will be incremented
+until an error code is sent. [@TODO: Not implemented, see
+https://github.com/propublica/cookcountyjail2/issues/9]
+"""
+MAX_DEFAULT_JAIL_NUMBER = 400
 
 
 def get_secrets():
