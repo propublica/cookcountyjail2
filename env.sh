dc2700db17c900ebb9af9ef9bcbfbd9c2213de53
David Eads <davideads@gmail.com>
2017-06-08 14:27:53 -0400

    add simple env juggling, closes #2 (probably)

---
+++ env.sh
@@ -0,0 +1,2 @@
+export cookcountyjail2_S3_BUCKET="s3://cookcountyjail.il.propublica.org"
+export cookcountyjail2_TARGET="dev"
---
+++ jailscraper/project_config.py
@@ -0,0 +1,19 @@
+# ProPublica specific configuration and utilities
+import os
+
+PROJECT_SLUG = 'cookcountyjail2'
+
+def get_secrets():
+    """
+    Get all environment variables associated with this project.
+    """
+    secrets = {}
+    for k, v in os.environ.items():
+        if k.startswith(PROJECT_SLUG):
+            new_k = k[len(PROJECT_SLUG) + 1:]
+            secrets[new_k] = v
+
+    return secrets
+
+SECRETS = get_secrets()
+S3_URL = '{0}/{1}'.format(SECRETS['S3_BUCKET'], SECRETS['TARGET'])
