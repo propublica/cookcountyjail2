2f20096fe38c0c902083a4a64b58d81ff895e0c5
David Eads <davideads@gmail.com>
2017-06-08 16:57:00 -0400

    basic parsing

--- jailscraper/items.py
+++ jailscraper/items.py
@@ -1,14 +1,23 @@
 # -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
+import dateparser
 import scrapy
 
+def strip(x):
+    if isinstance(x, list):
+        x = ''.join(x)
+    return x.strip()
 
-class JailscraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class InmateRecordItem(scrapy.Item):
+    Booking_Id = scrapy.Field()
+    Booking_Date = scrapy.Field()
+    Inmate_Hash = scrapy.Field()
+    Gender = scrapy.Field(serializer=strip)
+    Race = scrapy.Field(serializer=strip)
+    Height = scrapy.Field(serializer=strip)
+    Weight = scrapy.Field(serializer=strip)
+    Age_At_Booking = scrapy.Field()
+    Housing_Location = scrapy.Field(serializer=strip)
+    Charges = scrapy.Field(serializer=strip)
+    Bail_Amount = scrapy.Field(serializer=strip)
+    Court_Date = scrapy.Field(serializer=strip)
+    Court_Location = scrapy.Field(serializer=strip)
--- jailscraper/spiders/inmate_spider.py
+++ jailscraper/spiders/inmate_spider.py
@@ -3,51 +3,39 @@ import os
 import scrapy
 
 from datetime import date, timedelta
+from jailscraper import project_config
+from jailscraper.items import InmateRecordItem
 from urllib.parse import urlparse, parse_qs
 
 ONE_DAY = timedelta(days=1)
-INITIAL_DATA_FILE = 'data/raw_inmate_data/2016-07-24.csv'
-URL_TEMPLATE = 'http://www2.cookcountysheriff.org/search2/details.asp?jailnumber={0}'
 
 
 class InmatesSpider(scrapy.Spider):
     name = "inmates"
 
-    def __init__(self, category=None, *args, **kwargs):
-        super(InmatesSpider, self).__init__(*args, **kwargs)
-        self._today = date.today()
-        self._yesterday = date.today() - ONE_DAY
-        self._make_scrape_dir()
-        self._last_scraped_data = self._get_scraped_data()
-
-    def _make_scrape_dir(self):
-        self._data_dir = 'data/scraped_html/{0}'.format(self._today.strftime('%Y-%m-%d'))
-        if not os.path.exists(self._data_dir):
-            os.makedirs(self._data_dir)
-
-    def _get_scraped_data(self):
-        """Get data from last scrape"""
-        yesterdays_file = 'data/raw_inmate_data/{0}.csv'.format(self._yesterday.strftime('%Y-%m-%d'))
-
-        if not os.path.exists(yesterdays_file):
-            yesterdays_file = INITIAL_DATA_FILE
-            # @TODO if this path is taken, we need to crank through the intervening period
-
-        with open(yesterdays_file) as f:
-            reader = csv.DictReader(f)
-
-            # @TODO make urls here
-            return list(reader)
-
     def start_requests(self):
-        for row in self._last_scraped_data:
-            url = URL_TEMPLATE.format(row['Booking_Id'])
+        urls = [
+            'http://www2.cookcountysheriff.org/search2/details.asp?jailnumber=2017-0531001'
+        ]
+        for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
+        inmate = InmateRecordItem()
+        inmate['Booking_Id'] = self._parse_booking_id(response)
+        inmate['Booking_Date'] = response.selector.xpath('//div[@id="mainContent"]/table[2]/tr[2]/td[1]//text()').extract()
+        inmate['Race'] = response.selector.xpath('//div[@id="mainContent"]/table[1]/tr[2]/td[4]//text()').extract()
+        inmate['Height'] = response.selector.xpath('//div[@id="mainContent"]/table[1]/tr[2]/td[6]//text()').extract()
+        inmate['Weight'] = response.selector.xpath('//div[@id="mainContent"]/table[1]/tr[2]/td[7]//text()').extract()
+        inmate['Gender'] = response.selector.xpath('//div[@id="mainContent"]/table[1]/tr[2]/td[5]//text()').extract()
+        inmate['Housing_Location'] = response.selector.xpath('//div[@id="mainContent"]/table[2]/tr[2]/td[2]//text()').extract()
+        inmate['Bail_Amount'] = response.selector.xpath('//div[@id="mainContent"]/table[2]/tr[2]/td[4]//text()').extract()
+        inmate['Charges'] = response.selector.xpath('//div[@id="mainContent"]/table[2]/tr[4]/td[1]//text()').extract()
+        inmate['Court_Date'] = response.selector.xpath('//div[@id="mainContent"]/table[3]/tr[2]/td[1]//text()').extract()
+        inmate['Court_Location'] = response.selector.xpath('//div[@id="mainContent"]/table[3]/tr[2]/td[2]//text()').extract()
+        yield inmate
+
+    def _parse_booking_id(self, response):
         parsed_url = urlparse(response.url)
         qs = parse_qs(parsed_url.query)
-        filename = '{0}/{1}.html'.format(self._data_dir, qs['jailnumber'][0])
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file {0}'.format(filename))
+        return qs['jailnumber']
