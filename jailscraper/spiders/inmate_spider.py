fd0d62dbaaf6aee2b5960eaa0e91e3112dd0935b
David Eads <davideads@gmail.com>
2017-06-08 17:45:16 -0400

    alphabetize item fields

--- jailscraper/spiders/inmate_spider.py
+++ jailscraper/spiders/inmate_spider.py
@@ -22,17 +22,19 @@ class InmatesSpider(scrapy.Spider):
 
     def parse(self, response):
         inmate = InmateRecordItem()
-        inmate['Booking_Id'] = self._parse_booking_id(response)
-        inmate['Booking_Date'] = response.selector.xpath('//div[@id="mainContent"]/table[2]/tr[2]/td[1]//text()').extract()
-        inmate['Race'] = response.selector.xpath('//div[@id="mainContent"]/table[1]/tr[2]/td[4]//text()').extract()
-        inmate['Height'] = response.selector.xpath('//div[@id="mainContent"]/table[1]/tr[2]/td[6]//text()').extract()
-        inmate['Weight'] = response.selector.xpath('//div[@id="mainContent"]/table[1]/tr[2]/td[7]//text()').extract()
-        inmate['Gender'] = response.selector.xpath('//div[@id="mainContent"]/table[1]/tr[2]/td[5]//text()').extract()
-        inmate['Housing_Location'] = response.selector.xpath('//div[@id="mainContent"]/table[2]/tr[2]/td[2]//text()').extract()
+
         inmate['Bail_Amount'] = response.selector.xpath('//div[@id="mainContent"]/table[2]/tr[2]/td[4]//text()').extract()
+        inmate['Booking_Date'] = response.selector.xpath('//div[@id="mainContent"]/table[2]/tr[2]/td[1]//text()').extract()
+        inmate['Booking_Id'] = self._parse_booking_id(response)
         inmate['Charges'] = response.selector.xpath('//div[@id="mainContent"]/table[2]/tr[4]/td[1]//text()').extract()
         inmate['Court_Date'] = response.selector.xpath('//div[@id="mainContent"]/table[3]/tr[2]/td[1]//text()').extract()
         inmate['Court_Location'] = response.selector.xpath('//div[@id="mainContent"]/table[3]/tr[2]/td[2]//text()').extract()
+        inmate['Gender'] = response.selector.xpath('//div[@id="mainContent"]/table[1]/tr[2]/td[5]//text()').extract()
+        inmate['Height'] = response.selector.xpath('//div[@id="mainContent"]/table[1]/tr[2]/td[6]//text()').extract()
+        inmate['Housing_Location'] = response.selector.xpath('//div[@id="mainContent"]/table[2]/tr[2]/td[2]//text()').extract()
+        inmate['Race'] = response.selector.xpath('//div[@id="mainContent"]/table[1]/tr[2]/td[4]//text()').extract()
+        inmate['Weight'] = response.selector.xpath('//div[@id="mainContent"]/table[1]/tr[2]/td[7]//text()').extract()
+
         yield inmate
 
     def _parse_booking_id(self, response):
