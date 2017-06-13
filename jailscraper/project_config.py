7314411c057b76d6f041340c6d1a709db5659d0b
David Eads <davideads@gmail.com>
2017-06-13 10:11:56 -0400

    do a simple test to see if AWS access actually works

--- jailscraper/project_config.py
+++ jailscraper/project_config.py
@@ -21,7 +21,34 @@ def get_secrets():
     return secrets
 
 
+### Config
+
+# Shortname of project, for use in deployment and credential juggling.
+PROJECT_SLUG = 'cookcountyjail2'
+
+# URL template for inmate lookup tool.
+INMATE_URL_TEMPLATE = 'http://www2.cookcountysheriff.org/search2/details.asp?jailnumber={0}'
+
+"""Sets the maximum jail number to scan for by default.
+
+If the subsequent jail number returns a 2xx status code, it will be incremented
+until an error code is sent. [@TODO: Not implemented, see
+https://github.com/propublica/cookcountyjail2/issues/9]
+"""
+MAX_DEFAULT_JAIL_NUMBER = 400
+
+# Secrets
 SECRETS = get_secrets()
-S3_BUCKET = SECRETS['S3_BUCKET']
-TARGET = SECRETS['TARGET']
-S3_URL = 's3://{0}/{1}'.format(SECRETS['S3_BUCKET'], SECRETS['TARGET'])
+S3_BUCKET = SECRETS.get('S3_BUCKET')
+TARGET = SECRETS.get('TARGET')
+USE_S3_STORAGE = True
+USE_LOCAL_STORAGE = True
+
+# Check for S3 access
+if S3_BUCKET and USE_S3_STORAGE:
+    s3 = boto3.resource('s3')
+    try:
+        s3.meta.client.head_bucket(Bucket=S3_BUCKET)
+    except botocore.exceptions.ClientError:
+        # @TODO Complain!
+        USE_S3_STORAGE = False
