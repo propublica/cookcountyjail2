d04635cee19efd6eceacdae83626e997d0bc3eee
David Eads <davideads@gmail.com>
2017-06-13 12:08:36 -0400

    add failing tests for hasing and parameterize tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -7,41 +7,102 @@ def get_inmate(id):
         body = f.read()
     return InmatePage(body)
 
-inmate = get_inmate('2017-0608010')
+# @TODO is there a way to do this more elegantly with fixtures?
+testdata = (
+    (get_inmate('2015-0904292'), {
+            'age_at_booking': 21,
+            'bail_amount': '50,000.00',
+            'booking_id': '2015-0904292',
+            'charges':  """720 ILCS 5/11-9(a)(2)
+PUBLIC INDECENCY/LEWD EXPOSURE""",
+            'court_date': '2017-07-20',
+            'court_location':  """Criminal Courts Building
+Criminal Courts Building
 
-def test_bail_amount():
-    assert inmate.bail_amount == '50,000.00'
+,""",
+            'gender': 'Male',
+            'height': '509',
+            'housing_location': 'DIV9-3B-3105-1',
+            'inmate_hash': '7b7d440062f7cf7b3bc15a6c0fd543f4d84fd2e74d05969401085ce5c8d3e03b',
+            'race': 'BK',
+            'weight': '160',
 
-def test_age_at_booking():
-    assert inmate.age_at_booking == 21
-
-def test_housing_location():
-    assert inmate.housing_location == 'DIV2-D1-D-32'
-
-def test_race():
-    assert inmate.race == 'BK'
-
-def test_gender():
-    assert inmate.gender == 'Male'
-
-def test_height():
-    assert inmate.height == '507'
-
-def test_weight():
-    assert inmate.weight == '165'
-
-def test_booking_id():
-    assert inmate.booking_id == '2017-0608010'
-
-def test_court_date():
-    assert inmate.court_date == '2017-06-28'
-
-def test_charges():
-    assert inmate.charges == """720 ILCS 5/24-1.6(a)(3)(a)(5)
-AGG UUW/LOADED PISTOL, REVOLVER, HANDGUN-NO CCL"""
-
-def test_court_location():
-    assert inmate.court_location == """Markham
+    }),
+    (get_inmate('2017-0608010'), {
+            'age_at_booking': 21,
+            'bail_amount': '50,000.00',
+            'booking_id': '2017-0608010',
+            'charges':  """720 ILCS 5/24-1.6(a)(3)(a)(5)
+AGG UUW/LOADED PISTOL, REVOLVER, HANDGUN-NO CCL""",
+            'court_date': '2017-06-28',
+            'court_location':  """Markham
 Markham
 
-,"""
+,""",
+            'gender': 'Male',
+            'height': '507',
+            'housing_location': 'DIV2-D1-D-32',
+            'inmate_hash': 'xxx',
+            'race': 'BK',
+            'weight': '165',
+     }),
+)
+
+
+@pytest.mark.parametrize("inmate,expected", testdata)
+def test_bail_amount(inmate,expected):
+    assert inmate.bail_amount == expected['bail_amount']
+
+
+@pytest.mark.parametrize("inmate,expected", testdata)
+def test_age_at_booking(inmate, expected):
+    assert inmate.age_at_booking == expected['age_at_booking']
+
+
+@pytest.mark.parametrize("inmate,expected", testdata)
+def test_housing_location(inmate, expected):
+    assert inmate.housing_location == expected['housing_location']
+
+
+@pytest.mark.parametrize("inmate,expected", testdata)
+def test_race(inmate, expected):
+    assert inmate.race == expected['race']
+
+
+@pytest.mark.parametrize("inmate,expected", testdata)
+def test_gender(inmate, expected):
+    assert inmate.gender == expected['gender']
+
+
+@pytest.mark.parametrize("inmate,expected", testdata)
+def test_height(inmate, expected):
+    assert inmate.height == expected['height']
+
+
+@pytest.mark.parametrize("inmate,expected", testdata)
+def test_weight(inmate, expected):
+    assert inmate.weight == expected['weight']
+
+
+@pytest.mark.parametrize("inmate,expected", testdata)
+def test_booking_id(inmate, expected):
+    assert inmate.booking_id == expected['booking_id']
+
+
+@pytest.mark.parametrize("inmate,expected", testdata)
+def test_court_date(inmate, expected):
+    assert inmate.court_date == expected['court_date']
+
+
+@pytest.mark.parametrize("inmate,expected", testdata)
+def test_charges(inmate, expected):
+    assert inmate.charges == expected['charges']
+
+
+@pytest.mark.parametrize("inmate,expected", testdata)
+def test_court_location(inmate, expected):
+    assert inmate.court_location == expected['court_location']
+
+@pytest.mark.parametrize("inmate,expected", testdata)
+def test_inmate_hash(inmate, expected):
+    assert inmate.inmate_hash == expected['inmate_hash']
