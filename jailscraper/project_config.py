87aaa193c501aabed1398e04aec88f59870f11cd
David Eads <davideads@gmail.com>
2017-06-13 10:23:51 -0400

    complain if s3 fails

--- jailscraper/project_config.py
+++ jailscraper/project_config.py
@@ -1,10 +1,20 @@
 """ProPublica specific configuration and utilities"""
 import boto3
 import botocore
+import logging
 import os
+
+# Short name of project, for use in deployment and credential juggling.
+PROJECT_SLUG = 'cookcountyjail2'
 
 
 ### Helpers
+
+logging.basicConfig()
+logger = logging.getLogger(PROJECT_SLUG)
+logger.setLevel(logging.INFO)
+
+logger.info('Loading {0} config').format(PROJECT_SLUG)
 
 def get_secrets():
     """Get all environment variables associated with this project.
@@ -23,9 +33,6 @@ def get_secrets():
 
 ### Config
 
-# Shortname of project, for use in deployment and credential juggling.
-PROJECT_SLUG = 'cookcountyjail2'
-
 # URL template for inmate lookup tool.
 INMATE_URL_TEMPLATE = 'http://www2.cookcountysheriff.org/search2/details.asp?jailnumber={0}'
 
@@ -42,13 +49,15 @@ SECRETS = get_secrets()
 S3_BUCKET = SECRETS.get('S3_BUCKET')
 TARGET = SECRETS.get('TARGET')
 USE_S3_STORAGE = True
-USE_LOCAL_STORAGE = True
+USE_LOCAL_STORAGE = False
 
-# Check for S3 access
+# Check for S3 access / @TODO factor into function and test
 if S3_BUCKET and USE_S3_STORAGE:
     s3 = boto3.resource('s3')
     try:
         s3.meta.client.head_bucket(Bucket=S3_BUCKET)
     except botocore.exceptions.ClientError:
-        # @TODO Complain!
+        logger.warning('Amazon Web Services is unreachable. Scraped pages will not be stored on Amazon S3.')
+        if not USE_LOCAL_STORAGE:
+            logger.warning('`USE_LOCAL_STORAGE` is disabled in `project_config.py` and S3 access is failing. Scraped pages will not be stored.')
         USE_S3_STORAGE = False
