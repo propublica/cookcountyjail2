bc4d5e4dcb205900e4ec577053584a9daf7657f9
David Eads <davideads@gmail.com>
2017-07-24 16:33:29 -0500

    hardcode un-targeted s3 url in scrape.sh

--- scrape.sh
+++ scrape.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-scrapy crawl inmates -t csv -o s3://cookcountyjail.il.propublica.org/dev/daily/`date +"%Y-%m-%d"`.csv
+scrapy crawl inmates -t csv -o s3://cookcountyjail.il.propublica.org/daily/`date +"%Y-%m-%d"`.csv
 
 scripts/cleanup.py
