ec90f14589e2edc180135e62b369985c1f994c35
David Eads <davideads@gmail.com>
2017-08-17 16:13:40 -0500

    filename should line up with data date

--- scrape.sh
+++ scrape.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-scrapy crawl inmates -t csv -o s3://cookcountyjail.il.propublica.org/daily/`date +"%Y-%m-%d"`.csv
+scrapy crawl inmates -t csv -o s3://cookcountyjail.il.propublica.org/daily/`date +"%Y-%m-%d" -d "yesterday"`.csv
 
 scripts/cleanup.py
